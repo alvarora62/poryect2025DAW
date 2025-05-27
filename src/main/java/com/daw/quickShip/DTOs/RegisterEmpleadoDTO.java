76d7a9ce877c8844fdf43e73681dc3ba9e377a3b
alvarora62 <alvarora62@educastur.es>
2025-05-27 20:41:06 +0200

    Created RegisterEmpleadoDTO

---
+++ src/main/java/com/daw/quickShip/DTOs/RegisterEmpleadoDTO.java
@@ -0,0 +1,30 @@
+package com.daw.quickShip.DTOs;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+
+/**
+ * DTO for creating a new empleado.
+ */
+@Schema(description = "Datos necesarios para registrar un empleado en el sistema")
+public record RegisterEmpleadoDTO(
+        Long id,
+
+        @NotEmpty(message = "El nombre no puede estar vacío")
+        String nombre,
+
+        @NotEmpty(message = "El DNI no puede estar vacío")
+        @Pattern(regexp = "^[0-9]{8}[A-Z]+$", message = "El dni debe tener un formato especifico")
+        String dni,
+
+        @NotEmpty(message = "El teléfono no puede estar vacío")
+        @Pattern(regexp = "^[0-9]{9}$", message = "El teléfono debe tener 9 dígitos")
+        String telefono,
+
+        @NotEmpty(message = "El email no puede estar vacío")
+        @Email(message = "Formato de email inválido")
+        String email
+)
+{ }
---
+++ src/main/java/com/daw/quickShip/DTOs/RegisterRepartidorDTO.java
@@ -0,0 +1,26 @@
+package com.daw.quickShip.DTOs;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record RegisterRepartidorDTO(
+
+        @NotBlank(message = "El nombre no puede estar vacío.")
+        String nombre,
+
+        @NotBlank(message = "El DNI no puede estar vacío.")
+        @Pattern(regexp = "\\d{8}[A-HJ-NP-TV-Z]", message = "Formato de DNI inválido.")
+        String dni,
+
+        @NotBlank(message = "El email no puede estar vacío.")
+        @Email(message = "Formato de email inválido.")
+        String email,
+
+        @NotBlank(message = "El teléfono no puede estar vacío.")
+        @Pattern(regexp = "\\d{9}", message = "Formato de teléfono inválido.")
+        String telefono,
+
+        String nombreEmpresa
+
+) {}
