3633b2b1b8d679c71f0a7cc242844bc2b8d1f7bb
alvarora62 <alvarora62@educastur.es>
2025-05-28 22:42:37 +0200

    Added comments

--- src/main/java/com/daw/quickShip/exceptions/FormatException.java
+++ src/main/java/com/daw/quickShip/exceptions/FormatException.java
@@ -3,6 +3,12 @@ package com.daw.quickShip.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Exception thrown when a format validation fails.
+ *
+ * <p>This exception results in a 400 Bad Request HTTP response.</p>
+ *
+ */
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class FormatException extends RuntimeException {
     public FormatException(String message) {
--- src/main/java/com/daw/quickShip/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/daw/quickShip/exceptions/ResourceNotFoundException.java
@@ -3,6 +3,12 @@ package com.daw.quickShip.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Exception thrown when something is not found.
+ *
+ * <p>This exception results in a 404 Not Found HTTP response.</p>
+ *
+ */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
