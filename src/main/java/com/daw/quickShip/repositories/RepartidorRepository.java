506327e81da214798aaa5803ce7d48647edfcdf0
alvarora62 <alvarora62@educastur.es>
2025-05-30 21:45:19 +0200

    Updated

--- src/main/java/com/daw/quickShip/repositories/RepartidorRepository.java
+++ src/main/java/com/daw/quickShip/repositories/RepartidorRepository.java
@@ -1,23 +1,23 @@
 package com.daw.quickShip.repositories;
 
 import com.daw.quickShip.entities.Repartidor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RepartidorRepository extends JpaRepository<Repartidor, Long> {
 
-    @Query("SELECT r FROM Repartidor r WHERE r.active = :isActive")
-    Page<Repartidor> findByIsActive(boolean isActive, Pageable pageable);
-
+    @Query("SELECT DISTINCT r.nombreEmpresa FROM Repartidor r WHERE r.nombreEmpresa IS NOT NULL")
+    List<String> findDistinctNombreEmpresa();
+    List<Repartidor> findByNombreEmpresa(String nombreEmpresa);
     Optional<Repartidor> findByDni(String dni);
 
     boolean existsByDni(String dni);
-
     boolean existsByEmail(String email);
+
+
 }
--- src/main/java/com/daw/quickShip/servicies/RepartidorService.java
+++ src/main/java/com/daw/quickShip/servicies/RepartidorService.java
@@ -1,14 +1,19 @@
 package com.daw.quickShip.servicies;
 
 import com.daw.quickShip.DTOs.RegisterRepartidorDTO;
+import com.daw.quickShip.DTOs.SelectRepartidorDTO;
 import com.daw.quickShip.entities.Repartidor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public interface RepartidorService {
 
     Page<Repartidor> listAll(Pageable pageable);
+    List<String> selectList();
+    List<Repartidor> findByNombreEmpresa(String nombreEmpresa);
 
     ResponseEntity<Void> create(RegisterRepartidorDTO registerRepartidorDTO);
     ResponseEntity<Void> update(String dni, RegisterRepartidorDTO registerRepartidorDTO);
