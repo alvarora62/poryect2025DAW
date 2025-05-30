f7440e9ed705040ef37394dc809b5fa815bd3836
alvarora62 <alvarora62@educastur.es>
2025-05-30 21:43:53 +0200

    Created find by Name

--- src/main/java/com/daw/quickShip/servicies/EstadoPedidoService.java
+++ src/main/java/com/daw/quickShip/servicies/EstadoPedidoService.java
@@ -2,9 +2,7 @@ package com.daw.quickShip.servicies;
 
 import com.daw.quickShip.entities.EstadoPedido;
 
-import java.util.List;
-
 public interface EstadoPedidoService {
 
-    List<EstadoPedido> findAll();
+    EstadoPedido findByName(String status);
 }
