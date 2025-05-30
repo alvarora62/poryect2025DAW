0c7b37b8d2310e3a1e9f8fdb793db7e4c9b002a3
alvarora62 <alvarora62@educastur.es>
2025-05-30 21:43:40 +0200

    Created find by Id

--- src/main/java/com/daw/quickShip/servicies/EstadoPedidoServiceImpl.java
+++ src/main/java/com/daw/quickShip/servicies/EstadoPedidoServiceImpl.java
@@ -1,11 +1,10 @@
 package com.daw.quickShip.servicies;
 
 import com.daw.quickShip.entities.EstadoPedido;
+import com.daw.quickShip.exceptions.ResourceNotFoundException;
 import com.daw.quickShip.repositories.EstadoPedidoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,8 +12,16 @@ public class EstadoPedidoServiceImpl implements EstadoPedidoService{
 
     private final EstadoPedidoRepository estadoPedidoRepository;
 
+    /**
+     * Finds an order status by its name.
+     *
+     * @param status the name of the status to find.
+     * @return the {@link EstadoPedido} matching the given name.
+     * @throws ResourceNotFoundException if no status with the given name exists.
+     */
     @Override
-    public List<EstadoPedido> findAll() {
-        return estadoPedidoRepository.findAll();
+    public EstadoPedido findByName(String status) {
+        return estadoPedidoRepository.findByNombre(status.toUpperCase())
+                .orElseThrow(() -> new ResourceNotFoundException("Estado not valid."));
     }
 }
