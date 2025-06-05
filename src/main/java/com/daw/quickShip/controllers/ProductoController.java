d9806b4a569c6cc279cf78886ea0354570d3f389
alvarora62 <alvarora62@educastur.es>
2025-06-05 22:45:40 +0200

    Fixes a typo in ProductoController

--- src/main/java/com/daw/quickShip/controllers/ProductoController.java
+++ src/main/java/com/daw/quickShip/controllers/ProductoController.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/pedidos")
+@RequestMapping("/api/producto")
 @CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class ProductoController {
